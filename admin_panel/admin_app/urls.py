9f5f8ec66f309757ff6deb40d7ef8803d2ce55ea
Tusar <faisalahmedtusar@gmail.com>
2021-02-16 18:55:00 +0600

    Add: Profile, Basic Table added

--- admin_panel/admin_app/urls.py
+++ admin_panel/admin_app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    path('', views.home, name='admin-home'),
+    path('profile/', views.profile, name='admin-profile'),
+    path('basic-table/', views.basic_table, name='admin-basic-table'),
 ]
--- admin_panel/admin_app/views.py
+++ admin_panel/admin_app/views.py
@@ -8,9 +8,16 @@ data = {
 }
 
 
-# Create your views here.
 def home(request):
     context = {
         'userName': 'admin'
     }
     return render(request, 'pixel_admin/pixel_html/dashboard.html', context)
+
+
+def profile(request):
+    return render(request, 'pixel_admin/pixel_html/profile.html')
+
+
+def basic_table(request):
+    return render(request, 'pixel_admin/pixel_html/basic-table.html')
